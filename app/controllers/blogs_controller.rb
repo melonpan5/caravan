f0c40e8e6a4895ccc8906a0731a63c43268e82e0
melonpan5 <hoshi2771@gmail.com>
2020-05-05 14:38:30 +0900

    [Finish]4-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,10 @@
 class BlogsController < ApplicationController
-  def index
-  end
-
+ 
   def show
   end
 
-  def edit
+  def index
+  	@blogs = Blog.all
   end
 
 
@@ -16,14 +15,13 @@ class BlogsController < ApplicationController
 
   def create
   	blog = Blog.new(blog_params)
+  	blog.save
     redirect_to blogs_path
   end
 
- 
   private 
   
   def blog_params
   params.require(:blog).permit(:title, :category, :body)
-
-end
+  end
 end
